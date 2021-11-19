a02335e357e38b31c070a9bf5537309fcf43da6b
betitoblancas <betitoo.x86@gmail.com>
2021-11-19 12:18:48 -0600

    version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Selectornumerico2Component } from './selectornumerico2/selectornumerico2.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyectoComponentes';
+  valor1: number = 0;
+  valor2: number = 0;
+  valor3: number = 0;
+  resultado: string = '';
+  mensaje='';
+  articulo: Object = [null];
+  articulos = [{codigo:1, descripcion:'papas', precio:10.55},
+               {codigo:2, descripcion:'manzanas', precio:12.10},
+               {codigo:3, descripcion:'melon', precio:52.30},
+               {codigo:4, descripcion:'cebollas', precio:17},
+               {codigo:5, descripcion:'calabaza', precio:20}
+              ];
+  http: any;
+  constructor() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+  }
+
+  ngOnInit() {
+    this.http.get("http://scratchya.com.ar/vue/datos.php")
+      .subscribe(
+        (        result: Object): void => {
+          this.articulo = result;
+        },
+        (        error: any): void => {
+          console.log('problemas');
+        }
+      );
+  }
+  
+  @ViewChild('selector1') selector1!: Selectornumerico2Component;
+  @ViewChild('selector2') selector2!: Selectornumerico2Component;  
+
+  fijarSelector1(valor:number) {
+    this.selector1.fijar(valor);
+  }
+
+  fijarSelector2(valor:number) {
+    this.selector2.fijar(valor);
+  }
+
+
+  actualizar(t: string) {
+    this.mensaje = t + '(se actualiza cada 10 segundos)';
+  }
+  
+
+  retornarAleatorio() {
+    return Math.trunc(Math.random() * 6) + 1;
+  }
+
+  tirar() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+    if (this.valor1==this.valor2 && this.valor1==this.valor3)    
+      this.resultado='Ganó';
+    else
+      this.resultado='Perdió';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DadoComponent } from './dado/dado.component';
+import { CronometroComponent } from './cronometro/cronometro.component';
+import { SelectornumericoComponent } from './selectornumerico/selectornumerico.component';
+import { Selectornumerico2Component } from './selectornumerico2/selectornumerico2.component';
+import { ListadoarticulosComponent } from './listadoarticulos/listadoarticulos.component';
+import { JsonComponent } from './json/json.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DadoComponent,
+    CronometroComponent,
+    SelectornumericoComponent,
+    Selectornumerico2Component,
+    ListadoarticulosComponent,
+    JsonComponent
   ],
   imports: [
     BrowserModule
